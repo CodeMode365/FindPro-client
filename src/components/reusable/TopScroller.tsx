7d086a90a082e828ce7c28e519f979e09a3fb64f
Pabin234 <codemode365@gmail.com>
2024-02-29 20:05:28 +0545

    inital hidden

--- src/components/reusable/TopScroller.tsx
+++ src/components/reusable/TopScroller.tsx
@@ -1,16 +1,23 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { ArrowUp } from "lucide-react";
 
 const TopScroller = () => {
+  const [showBtn, setShowBtn] = useState(false);
+
+  useEffect(() => {
+    setShowBtn(true);
+  }, []);
+
   const scrollToTop = () => {
     window.scrollTo({
       top: 0,
       behavior: "smooth",
     });
   };
+  if (!showBtn) return null;
   return (
     <Button
       size={"icon"}
