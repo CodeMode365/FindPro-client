bafe66008677fc08ed88d410e79a257273524c0f
Pabin234 <codemode365@gmail.com>
2024-03-01 09:07:40 +0545

    improvization

--- src/app/about/_components/Gallery/index.tsx
+++ src/app/about/_components/Gallery/index.tsx
@@ -4,15 +4,13 @@ import GalleryCarousel from "./GalleryCarousel";
 const Gallery = () => {
   return (
     <>
-      <div className="flex flex-col text-center w-full mb-10">
+      <div className="flex flex-col text-center w-full mb-10 px-5">
         <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
           Our <span className="text-primary">Gallery</span>
         </h1>
-        <p className="lg:w-2/3 mx-auto leading-relaxed text-base">
+        <p className="lg:w-2/3 mx-auto leading-relaxed text-base text-gray-600">
           Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical
-          gentrify, subway tile poke farm-to-table. Franzen you probably haven&apos;t
-          heard of them man bun deep jianbing selfies heirloom prism food truck
-          ugh squid celiac humblebrag.
+          gentrify, subway tile poke farm-to-table.
         </p>
       </div>
 
