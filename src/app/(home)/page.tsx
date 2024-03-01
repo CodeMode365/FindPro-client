5e008d7ecba13de772bf8a1623e290195c0a8053
Pabin234 <codemode365@gmail.com>
2024-03-01 09:04:48 +0545

    migration import

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,16 +1,16 @@
 import Hero from "./_components/Hero";
 import Populars from "./_components/Populars";
-import Categories from "./_components/Categories";
 import FAQ from "./_components/FAQ";
 import Trending from "./_components/Trending";
+import CategoryCarousel from "./_components/Categories/CategoryCarousel";
 
 const Home = () => {
   return (
     <>
       <Hero />
       <div className="w-10/12 mx-auto">
-        <Categories direction="forward"  />
-        <Categories direction="backward" />
+        <CategoryCarousel direction="forward" />
+        <CategoryCarousel direction="backward" />
       </div>
       <Populars />
       <Trending />
