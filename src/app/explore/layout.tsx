323491a73e022e0fd84ca3079aa5e82e46cc39ec
Pabin234 <codemode365@gmail.com>
2024-03-01 09:06:07 +0545

    styling for small screen

--- src/app/explore/layout.tsx
+++ src/app/explore/layout.tsx
@@ -4,9 +4,9 @@ import Filter from "./_components/Filter";
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main>
-      <div className="mx-auto max-w-screen-xl px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16  flex md:flex-row flex-col ">
+      <div className="mx-auto py-8 sm:px-6 sm:py-12 lg:py-16  flex md:flex-row flex-col ">
         <aside className="w-full md:w-[25%] border-x p-2">
-          <div className="sticky top-5">
+          <div className="sticky top-24">
             <Filter />
           </div>
         </aside>
--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -14,14 +14,16 @@ import BannerCarousel from "./_components/Banner/BannerCrousel";
 
 const Explore = () => {
   return (
-    <div className="w-full ml-4 p-2 grid xs:grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6">
-      <div className="w-full h-auto col-span-full rounded-md shadow-lg">
+    <div className="w-full md:ml-4 md:p-2 ">
+      <div className=" h-auto col-span-full rounded-md shadow-lg mb-4">
         <BannerCarousel />
       </div>
 
-      <Professionals />
+      <div className="grid grid-cols-2 xs:grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-6  px-4 md:px-0 my-4">
+        <Professionals />
+      </div>
 
-      <div className="w-full col-span-full">
+      <div className="col-span-full">
         <Pagination className="mx-auto ">
           <PaginationContent>
             <PaginationItem>
