f7bc179590fd9f5040b9b6c64ee26459cd502bb8
Pabin234 <codemode365@gmail.com>
2024-03-02 14:54:16 +0545

    pulse animation on loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -4,14 +4,16 @@ import { PulseLoader, RotateLoader } from "react-spinners";
 const loading = () => {
   return (
     <div className="h-[80vh] w-full flex items-center justify-center">
-      <RotateLoader
-        className="text-primary"
-        color={"#0ea5e9"}
-        size={10}
-        loading={true}
-        aria-label="Loading Spinner"
-        data-testid="loader"
-      />
+      <div className="animate-pulse">
+        <RotateLoader
+          className="text-primary animate-ping"
+          color={"#0ea5e9"}
+          size={10}
+          loading={true}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        />
+      </div>
     </div>
   );
 };
