a69de4d6f7aa3cf884a5775ba6576ba6862866bd
Pabin234 <codemode365@gmail.com>
2024-03-03 08:07:16 +0545

    dummy data

---
+++ src/components/dummy/populars.ts
@@ -0,0 +1,37 @@
+import { Camera, ClipboardPlus, File, Home, HomeIcon, Music, PartyPopper } from "lucide-react";
+
+const content =
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, reiciendis recusandae nemo unde culpa neque ipsam laborum ratione quas magnam voluptatem?";
+
+export const popularsdata = [
+  {
+    Icon: ClipboardPlus,
+    title: "Health Assistance",
+    content,
+  },
+  {
+    Icon: Home,
+    title: "Home Services",
+    content,
+  },
+  {
+    Icon: Music,
+    title: "Musician",
+    content,
+  },
+  {
+    Icon: File,
+    title: "Content Writer",
+    content,
+  },
+  {
+    Icon: Camera,
+    title: "Photographer",
+    content,
+  },
+  {
+    Icon: PartyPopper,
+    title: "Digital Marketer",
+    content,
+  },
+];
