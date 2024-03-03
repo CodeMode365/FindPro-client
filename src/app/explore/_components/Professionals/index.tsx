237295e2fe56b1e3464e75e50acc1f9243875ab2
Pabin234 <codemode365@gmail.com>
2024-03-03 08:12:23 +0545

    dived

--- src/app/explore/_components/Professionals/index.tsx
+++ src/app/explore/_components/Professionals/index.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import ProCard from "./ProCard";
 
-
 const Professionals = () => {
   return (
     <>
       {Array.from({ length: 8 }).map((_, i) => (
-        <React.Fragment key={"item-no-" + i}>
+        <div key={"item-no-" + i}>
           <ProCard />
-        </React.Fragment>
+        </div>
       ))}
     </>
   );
