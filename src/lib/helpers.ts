97f2816b479b0c23132d82d90834d40a6c55de72
Pabin234 <codemode365@gmail.com>
2024-03-03 20:20:38 +0545

    toaste helper

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -1,4 +1,24 @@
+import toast from "react-hot-toast";
+
 export const generatePathname = (name: string, isUrl = true) => {
   const path = name.split(" ").join("-").toLocaleLowerCase();
   return isUrl ? encodeURI(path) : path;
+};
+
+export const toastGenerator = (
+  toastType: "loading" | "error" | "success",
+  message: string = "Please wait!"
+) => {
+  toast.remove();
+  switch (toastType) {
+    case "loading":
+      toast.loading(message);
+      return;
+    case "success":
+      toast.success(message);
+      return;
+    case "error":
+      toast.error(message);
+      return;
+  }
 };
