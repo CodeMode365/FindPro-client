2cfb5abe556448bc37466d2b883bca788dacb0d9
Pabin234 <codemode365@gmail.com>
2024-03-10 16:18:47 +0545

    working dashboard current path viewer

--- src/app/(protected)/dashboard/_components/PathView.tsx
+++ src/app/(protected)/dashboard/_components/PathView.tsx
@@ -1,10 +1,22 @@
-import React from "react";
+"use client";
+import { usePathname } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import _ from "lodash";
 
 const PathView = () => {
+  const pathname = usePathname();
+  const [currentPath, setCurrentPath] = useState("...");
+  useEffect(() => {
+    const path = pathname.split("/");
+    setCurrentPath(_.pull(path, "dashboard").join(""));
+  }, [pathname]);
   return (
     <div className="h-12 w-full col-span-full flex items-center">
       <h2 className="text-xl text-gray-700">
-        Dashboard / <span className="text-sm text-gray-500">Home</span>
+        Dashboard /{" "}
+        <span className="text-sm text-gray-500">
+          {currentPath !== "" ? currentPath : "Home"}
+        </span>
       </h2>
     </div>
   );
