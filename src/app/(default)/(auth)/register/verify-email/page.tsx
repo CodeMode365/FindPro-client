4c93ac4b5350a2ffaf310ea17d6469716007a687
Pabin234 <codemode365@gmail.com>
2024-03-12 08:59:52 +0545

    new verification code input

--- src/app/(default)/(auth)/register/verify-email/page.tsx
+++ src/app/(default)/(auth)/register/verify-email/page.tsx
@@ -1,42 +1,24 @@
 "use client";
 
-import { useEffect, useLayoutEffect, useRef, useState } from "react";
+import { useLayoutEffect, useState } from "react";
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
 import { toastGenerator } from "@/lib/helpers";
 import { permanentRedirect, useSearchParams } from "next/navigation";
 import { resendVerificationCode, verifyEmail } from "@/lib/server/actions/Auth";
 import { useLoadingStore } from "@/lib/states/loadingStore";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSeparator,
+  InputOTPSlot,
+} from "@/components/ui/input-otp";
 
 const EmailVerification = () => {
   const { isLoading, startLoading, stopLoading } = useLoadingStore();
   const [userEmail, setUserEmail] = useState<string | null>(null);
-  const inputRefs = useRef<HTMLInputElement[]>([]);
+  const [code, setCode] = useState("");
   const searchParams = useSearchParams();
-
-  const submitHandler = async (formData: FormData) => {
-    startLoading();
-    toastGenerator("loading", "Verifying your code!");
-    const entries = Object.fromEntries(formData.entries());
-    let code = "";
-    for (let key in entries) {
-      code += entries[key];
-    }
-    if (code.length !== 6) {
-      stopLoading();
-      return toastGenerator("error", "Invalid code!");
-    }
-    const res = await verifyEmail(userEmail, code);
-    if (res.success) {
-      stopLoading();
-      toastGenerator("success", "Email verified!");
-      return permanentRedirect("/");
-    } else {
-      toastGenerator("error", res.message);
-    }
-    stopLoading();
-  };
 
   const resendCode = async () => {
     startLoading();
@@ -50,47 +32,27 @@ const EmailVerification = () => {
     stopLoading();
   };
 
-  useEffect(() => {
+  const submitHandler = async () => {
+    startLoading();
+    toastGenerator("loading", "Verifying your code!");
+    if (code.length !== 6) {
+      stopLoading();
+      console.log(code);
+      return toastGenerator("error", "Invalid code!");
+    }
+    const res = await verifyEmail(userEmail, code);
+    if (res.success) {
+      stopLoading();
+      toastGenerator("success", "Email verified!");
+      return permanentRedirect("/");
+    } else {
+      toastGenerator("error", res.message);
+    }
     stopLoading();
-    const handleInput = (index: number) => (event: Event) => {
-      const input = event.target as HTMLInputElement;
-      if (input.value.length > 1) input.value = input.value.slice(1, 2);
-      else input.value = input.value.slice(0, 1);
-      if (input.value.length >= input.maxLength) {
-        const nextIndex = index + 1;
-        if (nextIndex < 6) {
-          inputRefs.current[nextIndex].focus();
-        } else {
-          input.blur();
-        }
-      }
-    };
-
-    const handleClear = (i: number) => (e: KeyboardEvent) => {
-      const input = e.target as HTMLInputElement;
-      if (e.key == "Backspace" && input.value == "") {
-        const prevIndex = i - 1;
-        if (prevIndex >= 0) {
-          inputRefs.current[prevIndex].focus();
-        }
-      }
-    };
-
-    inputRefs.current.forEach((ref, i) => {
-      ref.addEventListener("input", handleInput(i));
-      ref.addEventListener("keydown", handleClear(i));
-    });
-
-    return () => {
-      inputRefs.current.forEach((ref, i) => {
-        ref.removeEventListener("input", handleInput(i));
-        ref.removeEventListener("keydown", handleClear(i));
-        startLoading();
-      });
-    };
-  }, []);
+  };
 
   useLayoutEffect(() => {
+    stopLoading();
     const email = searchParams.get("email");
     if (email) {
       setUserEmail(email);
@@ -131,23 +93,34 @@ const EmailVerification = () => {
         <p className="text-primary">{searchParams.get("email")}</p>
       </div>
       <div className="flex">
-        {Array.from({ length: 6 }).map((_, id) => (
-          <Input
-            type="number"
-            key={`codeInput-${id + 1}`}
-            id={`codeInput-${id + 1}`}
-            name={`codeInput-${id + 1}`}
-            ref={(ele: HTMLInputElement) => {
-              if (inputRefs.current.length < 6)
-                return inputRefs.current.push(ele);
-            }}
-            disabled={isLoading}
-            className="mx-1 md:mx-2 h-8 w-8 text-sm sm:h-12 sm:w-12 md:h-16 md:w-16 border-primary md:text-4xl no-spinners text-center"
-            autoFocus={id == 0}
-            maxLength={1}
-            onKeyDown={(e) => {}}
-          />
-        ))}
+        <InputOTP
+          maxLength={6}
+          value={code}
+          onChange={(e) => setCode(e)}
+          render={({ slots }) => (
+            <>
+              <InputOTPGroup>
+                {slots.slice(0, 3).map((slot, index) => (
+                  <InputOTPSlot
+                    key={index}
+                    {...slot}
+                    className="h-8 w-8 text-sm sm:h-12 sm:w-12 md:h-16 md:w-16 border-primary md:text-4xl no-spinners text-center font-normal"
+                  />
+                ))}{" "}
+              </InputOTPGroup>
+              <InputOTPSeparator />
+              <InputOTPGroup>
+                {slots.slice(3).map((slot, index) => (
+                  <InputOTPSlot
+                    key={index}
+                    {...slot}
+                    className="h-8 w-8 text-sm sm:h-12 sm:w-12 md:h-16 md:w-16 border-primary md:text-4xl no-spinners text-center focus-within:hidden focus:hidden active:hidden"
+                  />
+                ))}
+              </InputOTPGroup>
+            </>
+          )}
+        />
       </div>
       <div className="w-full flex justify-between">
         <Button variant={"outline"} disabled={isLoading} onClick={resendCode}>
