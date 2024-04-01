d0c1ba85bafbc840aff2c90207714c26d83af2c4
Pabin234 <codemode365@gmail.com>
2024-04-01 16:24:47 +0545

    auth wrapper added

--- src/app/(protected)/dashboard/layout.tsx
+++ src/app/(protected)/dashboard/layout.tsx
@@ -1,21 +1,24 @@
+import DbAuthWrapper from "./_components/DbAuthWrapper";
 import PathView from "./_components/PathView";
 import Sidebar from "./_components/Sidebar";
 import Topbar from "./_components/Topbar";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className="grid grid-cols-12 max-h-screen overflow-hidden">
-      <div className="col-span-0 md:col-span-2 h-full">
-        <Sidebar />
-      </div>
-      <div className="col-span-12 md:col-span-10 max-h-screen overflow-y-auto">
-        <Topbar />
-        <div className="grid grid-cols-12 px-4">
-          <PathView />
-          {children}
+    <DbAuthWrapper>
+      <main className="grid grid-cols-12 max-h-screen overflow-hidden">
+        <div className="col-span-0 md:col-span-2 h-full">
+          <Sidebar />
         </div>
-      </div>
-    </main>
+        <div className="col-span-12 md:col-span-10 max-h-screen overflow-y-auto">
+          <Topbar />
+          <div className="grid grid-cols-12 px-4">
+            <PathView />
+            {children}
+          </div>
+        </div>
+      </main>
+    </DbAuthWrapper>
   );
 };
 
