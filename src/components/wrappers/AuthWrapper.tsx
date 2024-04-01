97f85177b08b939ba990d131988a18ee3f050ccf
Pabin234 <codemode365@gmail.com>
2024-04-01 16:21:17 +0545

    auth wrapper test

--- src/components/wrappers/AuthWrapper.tsx
+++ src/components/wrappers/AuthWrapper.tsx
@@ -1,8 +1,12 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { useAuthStore } from "@/lib/states/AuthStore";
 import axios from "axios";
+import { useRouter } from "next/navigation";
+import { createContext } from "vm";
+
+const AuthContext = createContext();
 
 const AuthWrapper = ({ children }: { children: React.ReactNode }) => {
   const { clearUserData, updateUserData, userData } = useAuthStore();
@@ -18,19 +22,25 @@ const AuthWrapper = ({ children }: { children: React.ReactNode }) => {
           `${process.env.NEXT_PUBLIC_SERVER_URL}/auth/`,
           { headers }
         );
+        return res;
         updateUserData(res.data.data);
       } catch (error) {
         if (axios.isAxiosError(error)) {
           console.error(error);
         }
-        localStorage.clear()
+        localStorage.clear();
         clearUserData();
       }
       console.log(userData);
     }
     authUser();
-  }, []);
-  return <>{children}</>;
+    console.log(userData);
+  }, [router]);
+  return (
+    <AuthContext.Provider value={{ user, setUser }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export default AuthWrapper;
